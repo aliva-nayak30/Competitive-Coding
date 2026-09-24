cdb76a6bb534dad4f9bda842ac9ca6f21cbac131
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-09-24 18:59:01 +0530

    Added Parantheis Checker

---
+++ GFG/ParanthesisChecker.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isBalanced(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                top++;
+                stack[top] = ch;
+            }
+            else {
+
+                if (top == -1) {
+                    return false;
+                }
+
+                if (ch == ')' && stack[top] != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && stack[top] != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && stack[top] != '[') {
+                    return false;
+                }
+
+                top--;
+            }
+        }
+
+        return top == -1;
+    }
+}
